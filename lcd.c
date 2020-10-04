30436038d41e2cfffcd89136048e018022e85272
thanhphong98 <thanhphongho1998@gmail.com>
2020-10-04 15:47:10 +0700

    add write data function

--- lcd.c
+++ lcd.c
@@ -33,7 +33,8 @@ typedef struct lcd {
 	lcd_driver_t 		driver;
 	lcd_comm_mode_t 	mode;
 	lcd_pin_t			pin;
-	write_func 			_write_func;
+	write_func 			_write_cmd;
+	write_func 			_write_data;
 	SemaphoreHandle_t	lock;
 } lcd_t;
 
@@ -75,11 +76,55 @@ stm_err_t _init_mode_4bit(lcd_pin_t pin) {
 	return STM_OK;
 }
 
-stm_err_t _write_cmd_4bit(lcd_pin_t pin, uint8_t data) {
+stm_err_t _write_cmd_4bit(lcd_pin_t pin, uint8_t cmd) {
+
+	bool bit_data;
+	uint8_t nibble_h = cmd >> 4 & 0x0F;
+	uint8_t nibble_l = cmd & 0x0F;
+
+	/* Set pin RS to write to command register */
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_rs, pin.gpio_num_rs, false), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+
+	/* Write high nibble */
+	bit_data = (nibble_h >> 0) & 0x01;
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_d4, pin.gpio_num_d4, bit_data), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	bit_data = (nibble_h >> 1) & 0x01;
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_d5, pin.gpio_num_d5, bit_data), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	bit_data = (nibble_h >> 2) & 0x01;
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_d6, pin.gpio_num_d6, bit_data), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	bit_data = (nibble_h >> 3) & 0x01;
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_d7, pin.gpio_num_d7, bit_data), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_en, pin.gpio_num_en, true), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	vTaskDelay(1 / portTICK_PERIOD_MS);
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_en, pin.gpio_num_en, false), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	vTaskDelay(1 / portTICK_PERIOD_MS);
+
+	bit_data = (nibble_l >> 0) & 0x01;
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_d4, pin.gpio_num_d4, bit_data), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	bit_data = (nibble_l >> 1) & 0x01;
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_d5, pin.gpio_num_d5, bit_data), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	bit_data = (nibble_l >> 2) & 0x01;
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_d6, pin.gpio_num_d6, bit_data), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	bit_data = (nibble_l >> 3) & 0x01;
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_d7, pin.gpio_num_d7, bit_data), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_en, pin.gpio_num_en, true), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	vTaskDelay(1 / portTICK_PERIOD_MS);
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_en, pin.gpio_num_en, false), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
+	vTaskDelay(1 / portTICK_PERIOD_MS);
+
+	return STM_OK;
+}
+
+stm_err_t _write_data_4bit(lcd_pin_t pin, uint8_t data) {
 
 	bool bit_data;
 	uint8_t nibble_h = data >> 4 & 0x0F;
 	uint8_t nibble_l = data & 0x0F;
+
+	/* Set pin RS to high to write to data register */
+	LCD_CHECK(!gpio_set_level(pin.gpio_port_rs, pin.gpio_num_rs, true), LCD_WRITE_CMD_ERR_STR, return STM_FAIL);
 
 	/* Write high nibble */
 	bit_data = (nibble_h >> 0) & 0x01;
@@ -123,12 +168,14 @@ lcd_handle_t lcd_init(lcd_cfg_t *config) {
 	LCD_CHECK(handle, LCD_INIT_ERR_STR, return NULL);
 
 	init_func _init_func;
-	write_func _write_func;
+	write_func _write_cmd;
+	write_func _write_data;
 
 	switch (config->mode) {
 	case LCD_COMM_MODE_4BIT:
 		_init_func = _init_mode_4bit;
-		_write_func = _write_cmd_4bit;
+		_write_cmd = _write_cmd_4bit;
+		_write_data = _write_data_4bit;
 		break;
 	case LCD_COMM_MODE_8BIT:
 		break;
@@ -140,51 +187,49 @@ lcd_handle_t lcd_init(lcd_cfg_t *config) {
 
 	LCD_CHECK(!_init_func(config->pin), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
 
-	LCD_CHECK(!_write_func(config->pin, 0x02), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
+	LCD_CHECK(!_write_cmd(config->pin, 0x02), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
 	vTaskDelay(LCD_TICK_DELAY_DEFAULT / portTICK_PERIOD_MS);
 
-	LCD_CHECK(!_write_func(config->pin, 0x28), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
+	LCD_CHECK(!_write_cmd(config->pin, 0x28), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
 	vTaskDelay(LCD_TICK_DELAY_DEFAULT / portTICK_PERIOD_MS);
 
-	LCD_CHECK(!_write_func(config->pin, 0x06), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
+	LCD_CHECK(!_write_cmd(config->pin, 0x06), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
 	vTaskDelay(LCD_TICK_DELAY_DEFAULT / portTICK_PERIOD_MS);
 
-	LCD_CHECK(!_write_func(config->pin, 0x0C), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
+	LCD_CHECK(!_write_cmd(config->pin, 0x0C), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
 	vTaskDelay(LCD_TICK_DELAY_DEFAULT / portTICK_PERIOD_MS);
 
-	LCD_CHECK(!_write_func(config->pin, 0x01), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
+	LCD_CHECK(!_write_cmd(config->pin, 0x01), LCD_INIT_ERR_STR, {_lcd_cleanup(handle); return NULL;});
 	vTaskDelay(LCD_TICK_DELAY_DEFAULT / portTICK_PERIOD_MS);
 
 	handle->size = config->size;
 	handle->driver = config->driver;
 	handle->mode = config->mode;
 	handle->pin = config->pin;
-	handle->_write_func = _write_func;
+	handle->_write_cmd = _write_cmd_4bit;
+	handle->_write_data = _write_data_4bit;
 	handle->lock = mutex_create();
 
 	return handle;
 }
 
 stm_err_t lcd_clear(lcd_handle_t handle) {
-	LCD_CHECK(!gpio_set_level(handle->pin.gpio_port_rs, handle->pin.gpio_num_rs, false), LCD_CLEAR_ERR_STR, return STM_FAIL);
-	handle->_write_func(handle->pin, 0x01);
+	handle->_write_cmd(handle->pin, 0x01);
 	vTaskDelay(2 / portTICK_PERIOD_MS);
 
 	return STM_OK;
 }
 
 stm_err_t lcd_home(lcd_handle_t handle) {
-	LCD_CHECK(!gpio_set_level(handle->pin.gpio_port_rs, handle->pin.gpio_num_rs, false), LCD_CLEAR_ERR_STR, return STM_FAIL);
-	handle->_write_func(handle->pin, 0x02);
+	handle->_write_cmd(handle->pin, 0x02);
 	vTaskDelay(2 / portTICK_PERIOD_MS);
 
 	return STM_OK;
 }
 
 stm_err_t lcd_write_string(lcd_handle_t handle, uint8_t *str) {
-	LCD_CHECK(!gpio_set_level(handle->pin.gpio_port_rs, handle->pin.gpio_num_rs, true), LCD_CLEAR_ERR_STR, return STM_FAIL);
 	while (*str) {
-		handle->_write_func(handle->pin, *str);
+		handle->_write_data(handle->pin, *str);
 		str++;
 	}
 
