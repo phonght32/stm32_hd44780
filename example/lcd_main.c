ab227dffca71732c3da3103315cbe61f8d422466
thanhphong98 <thanhphongho1998@gmail.com>
2020-11-15 22:12:02 +0700

    update example

--- example/lcd_main.c
+++ example/lcd_main.c
@@ -1,6 +1,6 @@
 // MIT License
 
-// Copyright (c) 2020 thanhphong98
+// Copyright (c) 2020 phonght32
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -38,11 +38,13 @@ lcd_hd44780_handle_t lcd_handle;
 static void example_task(void* arg)
 {
 
-    lcd_hd44780_pin_t lcd_pin = {
+    lcd_hd44780_hardware_info_t lcd_hw_info = {
         .gpio_port_rs = GPIO_PORT_A,
         .gpio_num_rs = GPIO_NUM_0,
-        .gpio_port_rw= GPIO_PORT_A,
-        .gpio_num_rw = GPIO_NUM_1,
+        .gpio_port_rw = -1,
+        .gpio_num_rw = -1,
+        // .gpio_port_rw = GPIO_PORT_A,
+        // .gpio_num_rw = GPIO_NUM_1,
         .gpio_port_en = GPIO_PORT_A,
         .gpio_num_en = GPIO_NUM_4,
         .gpio_port_d4 = GPIO_PORT_B,
@@ -58,20 +60,18 @@ static void example_task(void* arg)
     lcd_hd44780_cfg_t lcd_config = {
         .size = LCD_HD44780_SIZE_16_2,
         .mode = LCD_HD44780_COMM_MODE_4BIT,
-        .pin = lcd_pin,
+        .hw_info = lcd_hw_info,
     };
 
     lcd_handle = lcd_hd44780_init(&lcd_config);
-    lcd_hd44780_clear(lcd_handle);
-    lcd_hd44780_home(lcd_handle);
-    lcd_hd44780_write_string(lcd_handle, "phong");
+    lcd_hd44780_clear(lcd_handle);  
 
-    while(1)
-    {
-        lcd_hd44780_clear(lcd_handle);
+    while (1)
+    {    
         lcd_hd44780_home(lcd_handle);
-        lcd_hd44780_write_string(lcd_handle, "phong");
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        lcd_hd44780_write_string(lcd_handle, "  LCD with STM-IDF");
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -83,7 +83,7 @@ int main(void)
 
     /* Create task */
     xTaskCreate(example_task, "example_task", TASK_SIZE, NULL, TASK_PRIOR, NULL);
-    
+
     /* Start RTOS scheduler */
     vTaskStartScheduler();
 }
